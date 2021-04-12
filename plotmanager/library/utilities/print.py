6bf05c7e9ceca122f991a066b21e865f480b515f
Swar <swar.m.patel@gmail.com>
2021-04-11 22:15:49 -0400

    Redoing sort with start

--- plotmanager/library/utilities/print.py
+++ plotmanager/library/utilities/print.py
@@ -5,12 +5,12 @@ from datetime import datetime
 from plotmanager.library.utilities.processes import get_manager_processes
 
 
-def _get_row_info(pid, running_work, num):
+def _get_row_info(pid, running_work):
     work = running_work[pid]
     phase_times = work.phase_times
     elapsed_time = (datetime.now() - work.datetime_start)
     elapsed_time = pretty_print_time(elapsed_time.seconds)
-    row = [num, work.job.name if work.job else '?', pid, work.datetime_start.strftime('%Y-%m-%d %H:%M:%S'),
+    row = [work.job.name if work.job else '?', pid, work.datetime_start.strftime('%Y-%m-%d %H:%M:%S'),
            elapsed_time, work.current_phase, phase_times.get(1, ''), phase_times.get(2, ''), phase_times.get(3, ''),
            phase_times.get(4, ''), work.progress]
     return [str(cell) for cell in row]
@@ -25,22 +25,22 @@ def pretty_print_time(seconds):
 def print_table(jobs, running_work, next_log_check, stop_plotting):
     statuses = []
     headers = ['num', 'job', 'pid', 'start', 'elapsed_time', 'current', 'phase1', 'phase2', 'phase3', 'phase4', 'progress']
-    statuses.append(headers)
     added_pids = []
-    num = 1
     for job in jobs:
         for pid in job.running_work:
             if pid not in running_work:
                 continue
-            statuses.append(_get_row_info(pid, running_work, num))
-            num += 1
+            statuses.append(_get_row_info(pid, running_work))
             added_pids.append(pid)
     for pid in running_work.keys():
         if pid in added_pids:
             continue
-        statuses.append(_get_row_info(pid, running_work, num))
-        num += 1
+        statuses.append(_get_row_info(pid, running_work))
         added_pids.append(pid)
+    statuses.sort(key=lambda x: (x[3]), reverse=True)
+    for i in range(len(statuses)):
+        statuses[i] = [str(i+1)] + statuses[i]
+    statuses = [headers] + statuses
 
     max_characters = [0 for cell in statuses[0]]
     for row in statuses:
