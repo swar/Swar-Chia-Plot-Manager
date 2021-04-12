27ae6d2cdc04d2ecdfe79d2552c0319ab5c68938
Swar <swar.m.patel@gmail.com>
2021-04-11 22:35:49 -0400

    Adding prints to stateless-manager.py

--- stateless-manager.py
+++ stateless-manager.py
@@ -7,21 +7,27 @@ from plotmanager.library.utilities.jobs import has_active_jobs_and_work, load_jo
 from plotmanager.library.utilities.log import check_log_progress
 from plotmanager.library.utilities.processes import get_running_plots
 
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Getting config.yaml info...')
 chia_location, log_directory, config_jobs, log_check_seconds, max_concurrent = get_config_info()
+
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Loading jobs...')
 jobs = load_jobs(config_jobs)
 
 next_log_check = datetime.now()
 next_job_work = {}
 running_work = {}
 
+print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Getting running plots...')
 jobs, running_work = get_running_plots(jobs, running_work)
 
 while has_active_jobs_and_work(jobs):
     # CHECK LOGS FOR DELETED WORK
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Updating log process...')
     check_log_progress(jobs=jobs, running_work=running_work)
     next_log_check = datetime.now() + timedelta(seconds=log_check_seconds)
 
     # DETERMINE IF JOB NEEDS TO START
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Checking if work needs to start...')
     jobs, running_work, next_job_work, next_log_check = monitor_jobs_to_start(
         jobs=jobs,
         running_work=running_work,
@@ -32,4 +38,5 @@ while has_active_jobs_and_work(jobs):
         next_log_check=next_log_check,
     )
 
+    print(f'{datetime.now().strftime("%Y-%m-%d %H:%M:%S")}: Sleeping for {log_check_seconds} seconds...')
     time.sleep(log_check_seconds)
