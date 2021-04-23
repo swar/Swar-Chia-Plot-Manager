38c7c4cc5658512815122ce0a71c8242e8cec781
Alan <alan@leadiq.com>
2021-04-23 12:26:16 -0400

    Fix capitalization

--- plotmanager/library/utilities/commands.py
+++ plotmanager/library/utilities/commands.py
@@ -46,7 +46,7 @@ def start_manager():
     if not get_manager_processes():
         raise ManagerError('Failed to start Manager.')
 
-    send_notifications(title='Plot manager started', body='Plot Manager has Started on ' + socket.gethostname() +'...', settings=notification_settings)
+    send_notifications(title='Plot manager started', body='Plot Manager has started on ' + socket.gethostname() +'...', settings=notification_settings)
     print('Plot Manager has started...')
 
 
