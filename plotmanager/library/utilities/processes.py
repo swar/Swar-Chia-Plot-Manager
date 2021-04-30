e79710781d5f313186aae6e809b03f025ef1adb1
Swar <swar.m.patel@gmail.com>
2021-04-29 20:09:14 -0400

    Fixing MacOS bug where chia plot names are Python

--- plotmanager/library/utilities/processes.py
+++ plotmanager/library/utilities/processes.py
@@ -88,7 +88,7 @@ def get_chia_drives():
     drive_stats = {'temp': {}, 'temp2': {}, 'dest': {}}
     chia_executable_name = get_chia_executable_name()
     for process in psutil.process_iter():
-        if chia_executable_name not in process.name():
+        if chia_executable_name not in process.name() and 'python' not in process.name().lower():
             continue
         try:
             if 'plots' not in process.cmdline() or 'create' not in process.cmdline():
@@ -169,7 +169,7 @@ def get_running_plots(jobs, running_work):
     logging.info(f'Getting running plots')
     chia_executable_name = get_chia_executable_name()
     for process in psutil.process_iter():
-        if chia_executable_name not in process.name():
+        if chia_executable_name not in process.name() and 'python' not in process.name().lower():
             continue
         try:
             if 'plots' not in process.cmdline() or 'create' not in process.cmdline():
