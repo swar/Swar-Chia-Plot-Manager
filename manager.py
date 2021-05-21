18233d8991499a8c10afc2ad529f106e29effda8
Deviant <serg.chaplya@gmail.com>
2021-05-21 03:45:36 +0300

    Update manager.py

--- manager.py
+++ manager.py
@@ -3,6 +3,7 @@ import argparse
 from plotmanager.library.utilities.exceptions import InvalidArgumentException
 from plotmanager.library.utilities.commands import start_manager, stop_manager, view, json_output, analyze_logs
 
+IS_MANAGER = False
 
 parser = argparse.ArgumentParser(description='This is the central manager for Swar\'s Chia Plot Manager.')
 
